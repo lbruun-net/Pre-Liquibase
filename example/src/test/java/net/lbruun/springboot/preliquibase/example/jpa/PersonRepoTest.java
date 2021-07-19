9cb85b5551b1ebd87e8408965f990c6a1d1f4698
Lars Bruun-Hansen <lbruun@apache.org>
2021-07-19 13:07:35 +0200

    Added support for @DataJpaTest annotation
    - PreLiquibase is now one of the auto-configs which Spring Boot
      will include when the @DataJpaTest annotation is used in unit tests.
    
    - The example project now includes a unit test.
    
    - README has been updated with information about writing
      unit tests.

---
+++ example/src/test/java/net/lbruun/springboot/preliquibase/example/jpa/PersonRepoTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 lbruun.net.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.lbruun.springboot.preliquibase.example.jpa;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+/**
+ * Example Unit test. Not meant to be a show case for 
+ * best practice of writing Spring Boot JPA unit tests.
+ * 
+ * The main point here is that you can use the @DataJpaTest 
+ * annotation because it will include Pre-Liquibase auto-configuration
+ */
+@DataJpaTest
+public class PersonRepoTest {
+
+    @Autowired
+    private PersonRepo personRepo;
+
+
+    @Test
+    void injectedComponentsAreNotNull() {
+        assertThat(personRepo).isNotNull();
+    }
+    
+    @Test
+    void saveAndRetrievePerson() {
+        
+        Person person = new Person();
+        person.setPersonId(1001L);
+        person.setFirstName("John");
+        person.setLastName("Doe");
+        person.setBirthDate(LocalDate.of(1996, 12, 4));
+        personRepo.save(person);
+        
+        // See if we can find the person we just saved
+        Optional<Person> personOpt = personRepo.findById(1001L);
+        assertThat(personOpt).isNotEmpty();
+    }
+}
