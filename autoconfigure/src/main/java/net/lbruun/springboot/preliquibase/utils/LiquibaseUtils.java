efcff163c1e019f30ed1cd86959763903e5abb5f
Zorglube <zorglube@broncotoxique.com>
2022-06-21 11:22:41 +0200

    LiquibaseUtils

--- autoconfigure/src/main/java/net/lbruun/springboot/preliquibase/utils/LiquibaseUtils.java
+++ autoconfigure/src/main/java/net/lbruun/springboot/preliquibase/utils/LiquibaseUtils.java
@@ -15,30 +15,33 @@
  */
 package net.lbruun.springboot.preliquibase.utils;
 
+import static java.util.Objects.nonNull;
+
 import java.sql.Connection;
 import java.sql.SQLException;
+
 import javax.sql.DataSource;
+
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.DatabaseException;
 import net.lbruun.springboot.preliquibase.PreLiquibaseException;
+import net.lbruun.springboot.preliquibase.PreLiquibaseException.ResolveDbPlatformError;
 
 /**
  * Utility methods for Liquibase
- * 
+ *
  * @author lbruun
  */
 public class LiquibaseUtils {
-    
 
     private LiquibaseUtils() {
     }
-    
-    
+
     /**
      * Finds the Liquibase database {@code shortname} for a DataSource.
-     * 
+     *
      * <p>
      * Non-complete list of possible return values:
      * <ul>
@@ -59,22 +62,22 @@ public class LiquibaseUtils {
      * (These are the same values as those which can be used in a Liquibase
      * {@code dbms} Precondition. Refer to Liquibase documentation for further
      * information on possible values.)
-     * 
+     *
      * <p>
-     * The method invokes {@code liquibase.database.DatabaseFactory#findCorrectDatabaseImplementation()}
+     * The method invokes
+     * {@code liquibase.database.DatabaseFactory#findCorrectDatabaseImplementation()}
      * and therefore requires Liquibase library on the classpath.
-     * 
+     *
      * <p>
-     * The method works like this: The database is determined by connecting to
-     * it and retrieving {@link java.sql.DatabaseMetaData} which is then matched
-     * against known database platforms in Liquibase. If there's any error
-     * during this operation then {@link PreLiquibaseException} will be thrown.
-     * If there are no errors, but the type of database simply isn't supported
-     * by Liquibase then {@code "unsupported"} is returned. If all goes well a
-     * Liquibase database shortname is returned as per the list above. The
-     * connection used to determine the database type is closed again before the
-     * method exits.
-     * 
+     * The method works like this: The database is determined by connecting to it
+     * and retrieving {@link java.sql.DatabaseMetaData} which is then matched
+     * against known database platforms in Liquibase. If there's any error during
+     * this operation then {@link PreLiquibaseException} will be thrown. If there
+     * are no errors, but the type of database simply isn't supported by Liquibase
+     * then {@code "unsupported"} is returned. If all goes well a Liquibase database
+     * shortname is returned as per the list above. The connection used to determine
+     * the database type is closed again before the method exits.
+     *
      * @param dataSource input
      * @return Liquibase database shortname, always lower case, never null;
      * @throws PreLiquibaseException.ResolveDbPlatformError on all kinds of errors
@@ -85,27 +88,27 @@ public class LiquibaseUtils {
 
         // This gets convoluted and looks like Java 1.4 code. Ugly.
         // Mainly because the Liquibase classes do not support AutoCloseable.
-        
+
         // See SpringLiquibase.getDatabaseProductName() method from where
         // this code is pretty much copied.
         try {
             connection = dataSource.getConnection();
             database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
             return database.getShortName();
-        } catch (SQLException ex1) {
-            throw new PreLiquibaseException.ResolveDbPlatformError("Could not acquire connection for DataSource", ex1);
-        } catch (DatabaseException ex2) {
-             throw new PreLiquibaseException.ResolveDbPlatformError("Error while finding Liquibase Database implementation for DataSource", ex2);
-        } catch (Exception ex3) {
-             throw new PreLiquibaseException.ResolveDbPlatformError("Unexpected error while finding Liquibase Database implementation for DataSource", ex3);
+        } catch (final SQLException ex1) {
+            throw new ResolveDbPlatformError("Could not acquire connection for DataSource", ex1);
+        } catch (final DatabaseException ex2) {
+            throw new ResolveDbPlatformError("Error while finding Liquibase Database implementation for DataSource", ex2);
+        } catch (final Exception ex3) {
+            throw new ResolveDbPlatformError("Unexpected error while finding Liquibase Database implementation for DataSource", ex3);
         } finally {
-            if (database != null) {
+            if (nonNull(database)) {
                 try {
                     database.close(); // will also close underlying JDBC connection
-                } catch (DatabaseException ex) {
-                    throw new PreLiquibaseException.ResolveDbPlatformError("Error while closing connection for DataSource", ex);
+                } catch (final DatabaseException ex) {
+                    throw new ResolveDbPlatformError("Error while closing connection for DataSource", ex);
                 }
-            } else if (connection != null) {
+            } else if (nonNull(connection)) {
                 try {
                     if (!connection.isClosed()) {
                         if (!connection.getAutoCommit()) {
@@ -113,11 +116,11 @@ public class LiquibaseUtils {
                         }
                         connection.close();
                     }
-                } catch (SQLException ex) {
+                } catch (final SQLException ex) {
                     throw new PreLiquibaseException.ResolveDbPlatformError("Error while closing connection for DataSource", ex);
                 }
             }
         }
     }
-    
+
 }
