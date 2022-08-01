b6c8edf2aa5f55a6f0c142c9a147094dc3418abc
Zorglube <zorglube@broncotoxique.com>
2022-08-01 16:19:30 +0200

    Rebase plus unit tests

--- autoconfigure/src/main/java/net/lbruun/springboot/preliquibase/PreLiquibaseProperties.java
+++ autoconfigure/src/main/java/net/lbruun/springboot/preliquibase/PreLiquibaseProperties.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,161 +26,161 @@ import org.springframework.validation.annotation.Validated;
 
 /**
  * Properties for Pre-Liquibase module.
- * 
+ *
  * @author lbruun
  */
 @Validated
 @ConfigurationProperties(prefix = PROPERTIES_PREFIX)
 public class PreLiquibaseProperties {
 
-    public static final String PROPERTIES_PREFIX = "preliquibase";
+  public static final String PROPERTIES_PREFIX = "preliquibase";
     public static final String DEFAULT_SCRIPT_LOCATION
             = "classpath:preliquibase/";
-    
-    private boolean enabled = true;
-    
-    /**
+
+  private boolean enabled = true;
+
+  /**
      * Database platform code to use when choosing which SQL script files
      * to execute (such as {@code preliquibase/${dbPlatformCode}.sql}). 
-     */
-    private String dbPlatformCode;
+   */
+  private String dbPlatformCode;
 
-    /**
-     * SQL script resource references.
-     */
-    @NotEmpty(message="sqlScriptReferences must not be empty")
-    private List<String> sqlScriptReferences = Collections.singletonList(DEFAULT_SCRIPT_LOCATION);
+  /**
+   * SQL script resource references.
+   */
+  @NotEmpty(message = "sql-script-references must not be empty")
+  private List<String> sqlScriptReferences = Collections.singletonList(DEFAULT_SCRIPT_LOCATION);
 
-    /**
-     * Whether to stop if an error occurs while executing the SQL script.
-     */
-    private boolean continueOnError = false;
+  /**
+   * Whether to stop if an error occurs while executing the SQL script.
+   */
+  private boolean continueOnError = false;
 
-    /**
-     * Statement separator in SQL initialization scripts.
-     */
-    private String separator = ";";
+  /**
+   * Statement separator in SQL initialization scripts.
+   */
+  private String separator = ";";
 
-    /**
-     * SQL scripts encoding.
-     */
-    private Charset sqlScriptEncoding = StandardCharsets.UTF_8;
-
-    
-    /**
-     * Get the 'enabled' setting (if the module is enabled or not).
-     * @see #setEnabled(boolean) 
-     * @return 
-     */
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    /**
-     * Disables or enables module. Default is {@code true} (module is enabled).
-     * @param enabled 
-     */
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-    
-
-    /**
-     * Gets 'continueOnError' setting.
-     * @see #setContinueOnError(boolean) 
-     */
-    public boolean isContinueOnError() {
-        return this.continueOnError;
-    }
+  /**
+   * SQL scripts encoding.
+   */
+  private Charset sqlScriptEncoding = StandardCharsets.UTF_8;
 
 
-    /**
+  /**
+   * Get the 'enabled' setting (if the module is enabled or not).
+   * @see #setEnabled(boolean)
+   * @return
+   */
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  /**
+   * Disables or enables module. Default is {@code true} (module is enabled).
+   * @param enabled
+   */
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+
+  /**
+   * Gets 'continueOnError' setting.
+   * @see #setContinueOnError(boolean)
+   */
+  public boolean isContinueOnError() {
+    return continueOnError;
+  }
+
+
+  /**
      * Sets whether to stop if an error occurs while executing the SQL script.
      * Default value is: {@code false}.
-     */
-    public void setContinueOnError(boolean continueOnError) {
-        this.continueOnError = continueOnError;
-    }
+   */
+  public void setContinueOnError(boolean continueOnError) {
+    this.continueOnError = continueOnError;
+  }
 
-    
-    /**
-     * Gets 'separator' setting.
-     * @see #setSeparator(java.lang.String) 
-     */
-    public String getSeparator() {
-        return this.separator;
-    }
 
-    /**
+  /**
+   * Gets 'separator' setting.
+   * @see #setSeparator(java.lang.String)
+   */
+  public String getSeparator() {
+    return separator;
+  }
+
+  /**
      * Sets statement separator in SQL scripts. 
      * Defaults to semi-colon if not set.
-     */
-    public void setSeparator(String separator) {
-        this.separator = separator;
-    }
+   */
+  public void setSeparator(String separator) {
+    this.separator = separator;
+  }
 
-    /**
-     * Gets 'sqlScriptEncoding'.
-     * @see #setSqlScriptEncoding(java.nio.charset.Charset) 
-     * @return 
-     */
-    public Charset getSqlScriptEncoding() {
-        return this.sqlScriptEncoding;
-    }
+  /**
+   * Gets 'sqlScriptEncoding'.
+   * @see #setSqlScriptEncoding(java.nio.charset.Charset)
+   * @return
+   */
+  public Charset getSqlScriptEncoding() {
+    return sqlScriptEncoding;
+  }
 
-    /**
+  /**
      * Sets the file encoding for SQL script file.
      * Defaults to {@code UTF-8} if not set.
-     * @param sqlScriptEncoding 
-     */
-    public void setSqlScriptEncoding(Charset sqlScriptEncoding) {
-        this.sqlScriptEncoding = sqlScriptEncoding;
-    }
+   * @param sqlScriptEncoding
+   */
+  public void setSqlScriptEncoding(Charset sqlScriptEncoding) {
+    this.sqlScriptEncoding = sqlScriptEncoding;
+  }
 
-    /**
-     * Gets 'dbPlatformCode'
-     * 
-     * <p>
-     * Note that this is an arbitrary value. It can be any string value.
-     * 
-     * @see #setDbPlatformCode(java.lang.String) 
-     * @return database platform code or {@code null} if the value isn't set. 
-     * 
-     */
-    public String getDbPlatformCode() {
-        return dbPlatformCode;
-    }
+  /**
+   * Gets 'dbPlatformCode'
+   * 
+   * <p>
+   * Note that this is an arbitrary value. It can be any string value.
+   * 
+   * @see #setDbPlatformCode(java.lang.String)
+   * @return database platform code or {@code null} if the value isn't set.
+   * 
+   */
+  public String getDbPlatformCode() {
+    return dbPlatformCode;
+  }
 
-    /**
+  /**
      * Sets the db engine code to use when finding which
      * SQL script to execute, as in {@code preliquibase/${dbEngineCode}.sql}}
-     * 
-     * <p>
+   * 
+   * <p>
      * Setting this value explicitly overrides the database platform
      * auto-detection. The value can any value; it will not be
      * validated.
-     * @param dbPlatformCode 
-     */
-    public void setDbPlatformCode(String dbPlatformCode) {
-        this.dbPlatformCode = dbPlatformCode;
-    }
+   * @param dbPlatformCode
+   */
+  public void setDbPlatformCode(String dbPlatformCode) {
+    this.dbPlatformCode = dbPlatformCode;
+  }
 
-    /**
-     * Gets the sqlScriptReferences
-     * @see #setSqlScriptReferences(java.util.List) 
-     * @return 
-     */
-    public List<String> getSqlScriptReferences() {
-        return sqlScriptReferences;
-    }
+  /**
+   * Gets the sqlScriptReferences
+   * @see #setSqlScriptReferences(java.util.List)
+   * @return
+   */
+  public List<String> getSqlScriptReferences() {
+    return sqlScriptReferences;
+  }
 
-    /**
-     * Sets location(s) of where to find the SQL script(s) to execute.
-     * 
-     * <p>
+  /**
+   * Sets location(s) of where to find the SQL script(s) to execute.
+   * 
+   * <p>
      * The value is interpreted slightly differently depending on its
      * content:
-     * <ul>
+   * <ul>
      *   <li>If the value is a Spring Resource textual reference which ends with {@code "/"}:
      *       In this case, the reference is expected to be a folder reference
      *       where SQL scripts can be found. From this folder:
@@ -190,13 +190,13 @@ public class PreLiquibaseProperties {
      *       "{@code default.sql}" is used. 
      *       If neither file is found then no action will be taken, similarly
      *       to {@link #setEnabled(boolean) disabling} the module.
-     *       <br>
-     *       Example values:<br>
-     *       {@code "classpath:my-folder/"} (load from classpath folder).<br>
-     *       {@code "file:c:/config/sql-scripts/"} (load from file system folder, Windows).<br>
-     *       {@code "file:/app/etc/sql-scripts/"} (load from file system folder, Linux).<br>
-     *       <br>
-     *   </li>
+   * <br>
+   * Example values:<br>
+   * {@code "classpath:my-folder/"} (load from classpath folder).<br>
+   * {@code "file:c:/config/sql-scripts/"} (load from file system folder, Windows).<br>
+   * {@code "file:/app/etc/sql-scripts/"} (load from file system folder, Linux).<br>
+   * <br>
+   * </li>
      *   <li>Otherwise: The value is interpreted as a comma-separated list of 
      *       Spring Resource textual references to <i>specific</i> SQL files. Each 
      *       script file will be executed in the order they are listed. Before
@@ -206,14 +206,14 @@ public class PreLiquibaseProperties {
      *       <br>
      *       Example value: {@code "file:/foo/bar/myscript1.sql,file:/foo/bar/myscript2.sql"}.
      *   </li>
-     * </ul>
-     * 
-     * <p>
-     * Default value: {@link #DEFAULT_SCRIPT_LOCATION}.
-     * 
-     * @param sqlScriptReferences list of Spring Resource references.
-     */
-    public void setSqlScriptReferences(List<String> sqlScriptReferences) {
-        this.sqlScriptReferences = sqlScriptReferences;
-    }
+   * </ul>
+   * 
+   * <p>
+   * Default value: {@link #DEFAULT_SCRIPT_LOCATION}.
+   * 
+   * @param sqlScriptReferences list of Spring Resource references.
+   */
+  public void setSqlScriptReferences(List<String> sqlScriptReferences) {
+    this.sqlScriptReferences = sqlScriptReferences;
+  }
 }
--- autoconfigure/src/test/java/net/lbruun/springboot/preliquibase/PreLiquibaseAutoConfigurationTest.java
+++ autoconfigure/src/test/java/net/lbruun/springboot/preliquibase/PreLiquibaseAutoConfigurationTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,30 +15,30 @@
  */
 package net.lbruun.springboot.preliquibase;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 import javax.sql.DataSource;
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.autoconfigure.AutoConfigurations;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.jdbc.EmbeddedDataSourceConfiguration;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.assertj.AssertableApplicationContext;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.boot.test.context.runner.ContextConsumer;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 
@@ -48,196 +48,179 @@ import org.springframework.core.io.Resource;
  * @author lbruun
  */
 @ExtendWith(OutputCaptureExtension.class)
-public class PreLiquibaseAutoConfigurationTest {
-    
+class PreLiquibaseAutoConfigurationTest {
 
-    private static final String JDBC_URL1 = "jdbc:hsqldb:mem:stdtest";
-    @BeforeEach
-    void init(TestInfo testInfo) {
-        System.out.println();
-        System.out.println();
-        System.out.println("Running test : " + testInfo.getDisplayName());
+  private static final String JDBC_URL1 = "jdbc:hsqldb:mem:stdtest";
+
+  @BeforeEach
+  void init(TestInfo testInfo) {
+    System.out.println("\n\nRunning test : " + testInfo.getDisplayName());
+  }
+
+  private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+      .withConfiguration(
+          AutoConfigurations.of(
+              PreLiquibaseAutoConfiguration.class,
+              LiquibaseAutoConfiguration.class))
+      .withPropertyValues("spring.datasource.generate-unique-name=true");
+
+  @Test
+  void mainTest() {
+
+    // This will also - indirectly - test if PreLiquibase executes before Liquibase.
+    // If not, the schema 'myschema' will not have been created when Liquibase
+    // executes and Liquibase will therefore fail.
+
+    contextRunner.withUserConfiguration(EmbeddedDataSourceConfiguration.class)
+        .withPropertyValues(
+            "spring.datasource.url=" + JDBC_URL1,
+            "sql.script.schemaname=myschema",
+            "spring.liquibase.default-schema=myschema")
+        .run(assertPreLiquibase(preLiquibase -> {
+
+          // Assert that PreLiquibase has resolved the db platform correctly
+          assertThat(preLiquibase.getDbPlatformCode()).isEqualTo("hsqldb");
+
+          // Assert that something was executed.
+          assertThat(preLiquibase.hasExecutedScripts()).isTrue();
+
+          // Assert that only one script has executed
+          assertThat(preLiquibase.getUnfilteredResources()).hasSize(1);
+
+          // Assert which script was executed
+          assertThat(getScriptFileName(preLiquibase, 0)).endsWith("preliquibase/hsqldb.sql");
+
+        }));
+  }
+
+  @Test
+  void backsOffIfNotEnabledPreLiquibase() {
+    contextRunner
+        .withUserConfiguration(EmbeddedDataSourceConfiguration.class)
+        .withPropertyValues(
+            "preliquibase.enabled=false",
+            "spring.datasource.url=" + JDBC_URL1)
+        .run(context -> assertThat(context).doesNotHaveBean(PreLiquibase.class));
+  }
+
+  @Test
+  void backsOffIfNotEnabledLiquibase() {
+    contextRunner
+        .withUserConfiguration(EmbeddedDataSourceConfiguration.class)
+        .withPropertyValues(
+            "spring.liquibase.enabled=false",
+            "spring.datasource.url=" + JDBC_URL1)
+        .run(context -> assertThat(context).doesNotHaveBean(PreLiquibase.class));
+  }
+
+  @Test
+  void backsOffIfNoLiquibaseOnClasspath() {
+    contextRunner
+        .withUserConfiguration(EmbeddedDataSourceConfiguration.class)
+        .withPropertyValues("spring.datasource.url=" + JDBC_URL1)
+        .withClassLoader(new FilteredClassLoader("liquibase"))
+        .run(context -> assertThat(context).doesNotHaveBean(PreLiquibase.class));
+  }
+
+  @Test
+  void backsOffIfCustomDefinedPreLiquibase() {
+    contextRunner
+        .withUserConfiguration(EmbeddedDataSourceConfiguration.class, PreLiquibaseUserConfiguration.class)
+        .withPropertyValues(
+            "sql.script.schemaname=myschema",
+            "spring.datasource.url=" + JDBC_URL1)
+        .run(context -> {
+          assertThat(context).hasBean("customPreLiquibase");
+          assertThat(context).doesNotHaveBean("preLiquibase");
+        });
+
+  }
+
+  @Test
+  void sqlScriptsInCustomLocation() {
+
+    contextRunner
+        .withUserConfiguration(EmbeddedDataSourceConfiguration.class)
+        .withPropertyValues(
+            "spring.datasource.url=" + JDBC_URL1,
+            "preliquibase.sql-script-references=file:src/test/resources/preliquibase-customlocation/hsqldb.sql,file:src/test/resources/preliquibase-customlocation/default.sql",
+            "sql.script.schemaname=myschema",
+            "spring.liquibase.default-schema=myschema")
+        .run(assertPreLiquibase(preLiquibase -> {
+
+          // Assert that PreLiquibase has resolved the db platform correctly
+          assertThat(preLiquibase.getDbPlatformCode()).isEqualTo("hsqldb");
+
+          // Assert that something was executed.
+          assertThat(preLiquibase.hasExecutedScripts()).isTrue();
+
+          // Assert that two scripts has executed
+          assertThat(preLiquibase.getUnfilteredResources()).hasSize(2);
+
+          // Assert which script was executed
+          assertThat(getScriptFileName(preLiquibase, 0)).endsWith("preliquibase-customlocation/hsqldb.sql");
+          assertThat(getScriptFileName(preLiquibase, 1)).endsWith("preliquibase-customlocation/default.sql");
+
+        }));
+  }
+
+  // Utility methods
+
+  private ContextConsumer<AssertableApplicationContext> assertPreLiquibase(Consumer<PreLiquibase> consumer) {
+    return context -> {
+      assertThat(context).hasSingleBean(PreLiquibase.class);
+      final PreLiquibase preLiquibase = context.getBean(PreLiquibase.class);
+      consumer.accept(preLiquibase);
+    };
+  }
+
+  private String getScriptFileName(PreLiquibase preLiquibase, int scriptNo) {
+
+    final List<Resource> unfilteredResources = preLiquibase.getUnfilteredResources();
+    if (unfilteredResources == null || unfilteredResources.size() - 1 < scriptNo) {
+      throw new RuntimeException("No such scriptNo");
+    }
+    final Resource res = unfilteredResources.get(scriptNo);
+
+    try {
+      return res.getURL().getPath();
+    } catch (final IOException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  @Configuration(proxyBeanMethods = false)
+  static class LiquibaseDataSourceConfiguration {
+
+    @Bean
+    @Primary
+    DataSource normalDataSource() {
+      return DataSourceBuilder.create().url("jdbc:hsqldb:mem:normal").username("sa").build();
     }
 
-
-    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
-            .withConfiguration(AutoConfigurations.of(
-                    PreLiquibaseAutoConfiguration.class, 
-                    LiquibaseAutoConfiguration.class))
-            .withPropertyValues("spring.datasource.generate-unique-name=true");
-
-    
-    @Test
-    void mainTest() {
-        
-        // This will also - indirectly - test if PreLiquibase executes before Liquibase.
-        // If not, the schema 'myschema' will not have been created when Liquibase
-        // executes and Liquibase will therefore fail.
-
-        this.contextRunner
-                .withUserConfiguration(EmbeddedDataSourceConfiguration.class)
-                .withPropertyValues(
-                        "spring.datasource.url=" + JDBC_URL1,
-                        "sql.script.schemaname=myschema",
-                        "spring.liquibase.default-schema=myschema")
-                .run(assertPreLiquibase((preLiquibase) -> {
-                    
-                    // Assert that PreLiquibase has resolved the db platform correctly
-                    assertThat(preLiquibase.getDbPlatformCode()).isEqualTo("hsqldb");
-
-                    // Assert that something was executed.
-                    assertThat(preLiquibase.hasExecutedScripts()).isTrue();
-                                        
-                    // Assert that only one script has executed
-                    assertThat(preLiquibase.getUnfilteredResources()).hasSize(1);
-
-                    // Assert which script was executed
-                    assertThat(getScriptFileName(preLiquibase, 0)).endsWith("preliquibase/hsqldb.sql");
-
-                }));
-    }
-    
-    @Test
-    void backsOffIfNotEnabledPreLiquibase() {
-        this.contextRunner
-                .withUserConfiguration(EmbeddedDataSourceConfiguration.class)
-                .withPropertyValues(
-                        "preliquibase.enabled=false",
-                        "spring.datasource.url=" + JDBC_URL1)
-                .run((context) -> assertThat(context).doesNotHaveBean(PreLiquibase.class));
+    @LiquibaseDataSource
+    @Bean
+    DataSource liquibaseDataSource() {
+      return DataSourceBuilder.create().url("jdbc:hsqldb:mem:liquibasetest").username("sa").build();
     }
 
-    @Test
-    void backsOffIfNotEnabledLiquibase() {
-        this.contextRunner
-                .withUserConfiguration(EmbeddedDataSourceConfiguration.class)
-                .withPropertyValues(
-                        "spring.liquibase.enabled=false",
-                        "spring.datasource.url=" + JDBC_URL1)
-                .run((context) -> assertThat(context).doesNotHaveBean(PreLiquibase.class));
+  }
+
+  @Configuration(proxyBeanMethods = false)
+  @EnableConfigurationProperties({PreLiquibaseProperties.class})
+  static class PreLiquibaseUserConfiguration {
+
+    @Bean
+    public PreLiquibase customPreLiquibase(
+        Environment environment,
+        PreLiquibaseProperties properties,
+        ApplicationContext applicationContext) {
+
+      return new PreLiquibase(
+          environment,
+          DataSourceBuilder.create().url(JDBC_URL1).build(),
+          properties,
+          applicationContext);
     }
-
-    @Test
-    void backsOffIfNoLiquibaseOnClasspath() {
-        this.contextRunner
-                .withUserConfiguration(EmbeddedDataSourceConfiguration.class)
-                .withPropertyValues(
-                        "spring.datasource.url=" + JDBC_URL1)
-                .withClassLoader(new FilteredClassLoader("liquibase"))
-                .run((context) -> assertThat(context).doesNotHaveBean(PreLiquibase.class));
-    }
-
-    @Test
-    void backsOffIfCustomDefinedPreLiquibase() {
-        this.contextRunner
-                .withUserConfiguration(EmbeddedDataSourceConfiguration.class, PreLiquibaseUserConfiguration.class)
-                .withPropertyValues(
-                        "sql.script.schemaname=myschema",
-                        "spring.datasource.url=" + JDBC_URL1)               
-                .run((context) -> {
-                    assertThat(context).hasBean("customPreLiquibase");
-                    assertThat(context).doesNotHaveBean("preLiquibase");
-                });
-
-    }
-
-
-
-
-    @Test
-    void sqlScriptsInCustomLocation() {
-        
-        this.contextRunner
-                .withUserConfiguration(EmbeddedDataSourceConfiguration.class)
-                .withPropertyValues(
-                        "spring.datasource.url=" + JDBC_URL1,
-                        "preliquibase.sqlScriptReferences=file:src/test/resources/preliquibase-customlocation/hsqldb.sql,file:src/test/resources/preliquibase-customlocation/default.sql",
-                        "sql.script.schemaname=myschema",
-                        "spring.liquibase.default-schema=myschema")
-                .run(assertPreLiquibase((preLiquibase) -> {
-                    
-                    // Assert that PreLiquibase has resolved the db platform correctly
-                    assertThat(preLiquibase.getDbPlatformCode()).isEqualTo("hsqldb");
-
-                    // Assert that something was executed.
-                    assertThat(preLiquibase.hasExecutedScripts()).isTrue();
-                                        
-                    // Assert that two scripts has executed
-                    assertThat(preLiquibase.getUnfilteredResources()).hasSize(2);
-
-                    // Assert which script was executed
-                    assertThat(getScriptFileName(preLiquibase, 0)).endsWith("preliquibase-customlocation/hsqldb.sql");
-                    assertThat(getScriptFileName(preLiquibase, 1)).endsWith("preliquibase-customlocation/default.sql");
-
-                }));
-    }
-
-
-
-    
-    
-
-    // Utility methods    
-
-    private ContextConsumer<AssertableApplicationContext> assertPreLiquibase(Consumer<PreLiquibase> consumer) {
-        return (context) -> {
-            assertThat(context).hasSingleBean(PreLiquibase.class);
-            PreLiquibase preLiquibase = context.getBean(PreLiquibase.class);
-            consumer.accept(preLiquibase);
-        };
-    }
-
-    private String getScriptFileName(PreLiquibase preLiquibase, int scriptNo) {
-        
-        List<Resource> unfilteredResources = preLiquibase.getUnfilteredResources();
-        if (unfilteredResources == null || ((unfilteredResources.size() - 1) < scriptNo)) {
-            throw new RuntimeException("No such scriptNo");
-        }
-        Resource res = unfilteredResources.get(scriptNo);
-        
-        try {
-            return res.getURL().getPath();
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-    
-    @Configuration(proxyBeanMethods = false)
-    static class LiquibaseDataSourceConfiguration {
-
-        @Bean
-        @Primary
-        DataSource normalDataSource() {
-            return DataSourceBuilder.create().url("jdbc:hsqldb:mem:normal").username("sa").build();
-        }
-
-        @LiquibaseDataSource
-        @Bean
-        DataSource liquibaseDataSource() {
-            return DataSourceBuilder.create().url("jdbc:hsqldb:mem:liquibasetest").username("sa").build();
-        }
-
-    }
-
-    @Configuration(proxyBeanMethods = false)
-    @EnableConfigurationProperties({PreLiquibaseProperties.class})
-    static class PreLiquibaseUserConfiguration {
-
-        @Bean
-        public PreLiquibase customPreLiquibase(
-                Environment environment,
-                PreLiquibaseProperties properties,
-                ApplicationContext applicationContext) {
-
-            return new PreLiquibase(
-                    environment,
-                    DataSourceBuilder.create().url(JDBC_URL1).build(),
-                    properties,
-                    applicationContext);
-        }
-
-
-
-
-    }
+  }
 }
